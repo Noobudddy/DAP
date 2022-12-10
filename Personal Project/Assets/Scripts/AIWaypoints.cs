4e7fbdf8df7832a379cbd1ff1e2cfb6d393f118c
Noobudddy <n.bailon6002@gmail.com>
2022-12-09 16:44:25 -0800

    Revert "In Class work"
    This reverts commit 7a84ff07576217679c8da919df6695ea46b60d65, reversing
    changes made to 997bdad9cc2e4274c0327ddae51ccb2e4f2a86e8.

--- Personal Project/Assets/Scripts/AIWaypoints.cs
+++ Personal Project/Assets/Scripts/AIWaypoints.cs
@@ -2,11 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-<<<<<<<< HEAD:Personal Project/Assets/Scripts/AIWaypoints.cs
 public class AIWaypoints : MonoBehaviour
-========
-public class AllyController : MonoBehaviour
->>>>>>>> upstream/main:Personal Project/Assets/Scripts/AllyController.cs
 {
     // Start is called before the first frame update
     void Start()
--- Personal Project/Assets/Scripts/PlayerController.cs
+++ Personal Project/Assets/Scripts/PlayerController.cs
@@ -5,55 +5,32 @@ using UnityEngine.AI;
 
 public class PlayerController : MonoBehaviour
 {
-    private new Camera camera;
+    public Camera camera;
     private RaycastHit hit;
-    public NavMeshAgent agent;
+    private NavMeshAgent agent;
+    private string groundTag = "Ground";
 
     public float rotateSpeedMovement = 0.1f;
-    public float rotateVelocity;
-
-    private HeroCombat heroCombatScript;
+    float rotateVelocity;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
-        heroCombatScript = GetComponent<HeroCombat>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(heroCombatScript.targetedEnemy != null)
-        {
-            if(heroCombatScript.targetedEnemy.GetComponent<HeroCombat>() != null)
-            {
-                if (heroCombatScript.targetedEnemy.GetComponent<HeroCombat>().isHeroAlive)
-                {
-                    heroCombatScript.targetedEnemy = null;
-                }
-            }
-        }
-
         if (Input.GetMouseButtonDown(1))
         {
             Ray ray = camera.ScreenPointToRay(Input.mousePosition);
 
             if (Physics.Raycast(ray, out hit, Mathf.Infinity))
             {
-                if (hit.collider.tag == "Floor")
+                if (hit.collider.CompareTag(groundTag))
                 {
                     agent.SetDestination(hit.point);
-                    heroCombatScript.targetedEnemy = null;
-                    agent.stoppingDistance = 0;
-
-                    Quaternion rotationToLookAt = Quaternion.LookRotation(hit.point - transform.position);
-                    float rotationY = Mathf.SmoothDampAngle(transform.localEulerAngles.y,
-                        rotationToLookAt.eulerAngles.y,
-                        ref rotateVelocity,
-                        rotateSpeedMovement * (Time.deltaTime * 5));
-
-                    transform.eulerAngles = new Vector3(0, rotationY, 0);
                 }
             }
         }

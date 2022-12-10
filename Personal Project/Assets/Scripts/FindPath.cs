ebacf298445cd6635fa89236f614c6f2f0169d90
Noobudddy <n.bailon6002@gmail.com>
2022-12-09 17:32:44 -0800

    Home work
    minions function not working

---
+++ Personal Project/Assets/Scripts/FindPath.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FindPath : MonoBehaviour
+{
+
+    public NavMeshAgent myAgent;
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myAgent.SetDestination(target.transform.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

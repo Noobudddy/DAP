ccf8ec345b27eccb1dff636fc3001a98811e8c8d
Noobudddy <n.bailon6002@gmail.com>
2022-12-11 16:29:36 -0800

    home work
    Made ally and enemy minions move at start of game to designated area

--- Personal Project/Assets/Scripts/FindPath.cs
+++ Personal Project/Assets/Scripts/FindPath.cs
@@ -1,24 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.AI;
 
 public class FindPath : MonoBehaviour
 {
 
-    public NavMeshAgent myAgent;
-
-    public GameObject target;
+    public float speed;
+    public GameObject minion;
+    public Vector3 target = new Vector3(0, 0, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        myAgent.SetDestination(target.transform.position);
+        //target = GameObject.Find("");
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        minion.transform.position = Vector3.MoveTowards(transform.position, target, speed);
     }
 }

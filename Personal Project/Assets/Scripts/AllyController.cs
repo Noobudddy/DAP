0863c97cd58e0791da74f0e61c5210eaff3cc137
Noobudddy <n.bailon6002@gmail.com>
2022-12-13 15:23:29 -0800

    class work
    started ally controller script

--- Personal Project/Assets/Scripts/AllyController.cs
+++ Personal Project/Assets/Scripts/AllyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class AllyController : MonoBehaviour
 {
+
+    public bool isAlive;
+
     // Start is called before the first frame update
     void Start()
     {
--- Personal Project/Assets/Scripts/FindPath.cs
+++ Personal Project/Assets/Scripts/FindPath.cs
@@ -12,7 +12,7 @@ public class FindPath : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //target = GameObject.Find("");
+        
     }
 
     // Update is called once per frame
